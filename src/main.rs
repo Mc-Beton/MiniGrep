9203b7d414ea4807ee8598ab26b3e5c0a559afe9
Filip <el.filberto@gmail.com>
2022-12-31 15:53:44 +0100

    Refractor code after tests

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem with: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     if let Err(e) = minigrep::run(config) {
-        println!("App error: {e}");
         process::exit(1);
     }
 }
