a423b9edbe95631aa2b7de8e1fbf89cefaa5e9dc
Filip <el.filberto@gmail.com>
2023-01-12 16:00:34 +0100

    Upgrade the search method wit an Iterator

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub struct Config {
 impl Config {
     pub fn build(mut args: impl Iterator<Item = String>,) -> Result<Config, &'static str> {
         args.next();
-        
+
         let query = match args.next() {
             Some(arg) => arg,
             None => return Err("Didn't get a query string"),
@@ -45,14 +45,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
